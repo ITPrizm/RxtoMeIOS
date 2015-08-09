63f650f4d3bc5623ae3667b013664a2d8b303a2f
blasian <mikespearman.e@gmail.com>
2015-08-09 14:19:55 -0700

    Initial Commit

---
+++ RxToMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RxToMe
+//
+//  Created by Michael Spearman on 8/9/15.
+//  Copyright (c) 2015 Michael Spearman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RxToMe/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  RxToMe
+//
+//  Created by Michael Spearman on 8/9/15.
+//  Copyright (c) 2015 Michael Spearman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
